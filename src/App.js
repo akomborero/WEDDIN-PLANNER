28a8cd9c063b62e92edef2d61ba67e869f9fa397
akomborero <makomborerichidzviva@gmail.com>
2025-06-26 08:39:51 +0200

    wedding planner

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import EventPackages from "./pages/EventPackages"; // <--- ADDED THIS IMPORT
+import EventPackages from "./pages/EventPackages";
+import WeddingDashboard from "./pages/WeddingDashboard"; // <--- NEW IMPORT for the Dashboard!
+// Import other pages as you create them:
+// import OurStory from "./pages/OurStory";
+// import Testimonials from "./pages/Testimonials";
+// import Services from "./pages/Services";
+// import BookNow from "./pages/BookNow";
 
 function App() {
   return (
@@ -10,8 +16,14 @@ function App() {
       <Navbar /> {/* Your Navbar component */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/event-packages" element={<EventPackages />} /> {/* <--- ADDED THIS ROUTE */}
-        {/* Add other routes for other pages (e.g., /our-story, /contact, /our-testimonials) */}
+        <Route path="/event-packages" element={<EventPackages />} />
+        <Route path="/dashboard" element={<WeddingDashboard />} /> {/* <--- NEW ROUTE for the Dashboard! */}
+
+        {/* Add other routes for other pages: */}
+        {/* <Route path="/our-story" element={<OurStory />} /> */}
+        {/* <Route path="/testimonials" element={<Testimonials />} /> */}
+        {/* <Route path="/services" element={<Services />} /> */}
+        {/* <Route path="/book-now" element={<BookNow />} /> */}
       </Routes>
       {/* If your footer is a separate component, it would typically be rendered here, outside <Routes> */}
     </Router>
